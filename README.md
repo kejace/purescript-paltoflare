f6d763eb9781958c40b356e3f46b10c5518552f9
kejace <kejace@gmail.com>
2016-02-01 11:45:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Building
+```
+bower install
+pulp build -O -I test -m Test.Main -t html/main.js
+```
---
+++ test/Main.purs
@@ -0,0 +1,46 @@
+module Test.Main where
+
+import Prelude
+
+import Data.Array
+import Data.Maybe
+import Data.Monoid
+import Data.Foldable
+import Data.Int
+import Data.Traversable
+import Math (pow, sin, cos, pi, abs)
+
+import Control.Monad.Eff
+import DOM
+
+import Signal.DOM
+import Signal.Time
+import Signal.Channel
+
+import Text.Smolder.HTML as H
+import Text.Smolder.Markup as H
+import Text.Smolder.HTML.Attributes as A
+
+import Flare
+import Flare.Drawing
+import Flare.Smolder
+
+import Palto
+
+expr2 :: forall repr. (Mult repr, Expr repr) => Number -> Number -> Number -> repr Number
+expr2 x y z = (pint x `pmul` (pint y `padd` pint z))
+
+h :: Number -> Number -> Number -> H.MarkupM Unit
+h x y z = (H.div `H.with` (A.style $ "background-color: #f3f") $ H.text $ runStringify $ expr2 x y z) 
+       ++ (H.div `H.with` (A.style $ "background-color: #abc") $ H.text " = ")
+       ++ (H.div `H.with` (A.style $ "background-color: #ccc") $ H.text $ show (runEval $ expr2 x y z))
+
+ui0 :: forall a. (UI a) H.Markup
+ui0 = h <$> number "x" 5.0 
+        <*> number "y" 3.0
+        <*> number "z" 2.0
+
+-- Render everything to the DOM
+main :: forall a. Eff ( chan :: Chan, dom :: DOM | a) Unit
+main = do
+  runFlareHTML     "controls0"   "output0"  ui0
