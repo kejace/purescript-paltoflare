eef9f63dc789077e67a62a0c924be11d655d0128
kejace <kejace@gmail.com>
2017-04-14 07:48:05 -0700

    Fixed types and now compiles

--- test/Main.purs
+++ test/Main.purs
@@ -2,31 +2,17 @@ module Test.Main where
 
 import Prelude
 
-import Data.Array
-import Data.Maybe
-import Data.Monoid
-import Data.Foldable
-import Data.Int
-import Data.Traversable
-import Math (pow, sin, cos, pi, abs)
-
-import Control.Monad.Eff
-import DOM
-
+import DOM (DOM)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-import Signal.DOM
-import Signal.Time
-import Signal.Channel
+import Signal.Channel (CHANNEL)
 
 import Text.Smolder.HTML as H
 import Text.Smolder.Markup as H
 import Text.Smolder.HTML.Attributes as A
 
-import Flare
-import Flare.Drawing
-import Flare.Smolder
+import Flare (UI, number)
+import Flare.Smolder (runFlareHTML)
 
 import Palto
 
@@ -35,22 +21,17 @@ expr2 :: forall repr. Mult repr
       => Number -> Number -> Number -> repr Number
 expr2 x y z = (pint x `pmul` (pint y `padd` pint z))
 
-doc :: forall a e. H.Markup (a -> Eff (console :: CONSOLE | e) Unit)
-doc x y z = H.html $ do
-              H.head $ do
-                H.title $ H.text "OMG HAI LOL"
-
-h :: Number -> Number -> Number -> H.MarkupM Unit
+h :: forall a. Number -> Number -> Number -> H.MarkupM a Unit
 h x y z = (H.div `H.with` (A.style $ "background-color: #f3f") $ H.text $ runStringify $ expr2 x y z) 
        <> (H.div `H.with` (A.style $ "background-color: #abc") $ H.text " = ")
        <> (H.div `H.with` (A.style $ "background-color: #ccc") $ H.text $ show (runEval $ expr2 x y z))
 
-ui0 :: forall a. (UI a) H.Markup
+ui0 :: forall a e. UI e (H.MarkupM a Unit)
 ui0 = h <$> number "x" 5.0 
         <*> number "y" 3.0
         <*> number "z" 2.0
 
 -- Render everything to the DOM
-main :: forall a. Eff ( channel :: CHANNEL, dom :: DOM | a) Unit
+main :: Eff ( channel :: CHANNEL, dom :: DOM) Unit
 main = do
   runFlareHTML     "controls0"   "output0"  ui0
